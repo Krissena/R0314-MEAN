a6b7bd1b8e60e3bc556c05e33211e56982a3b0f4
Krissena <kristiina.laaksonen@outlook.com>
2020-02-12 20:56:08 +0200

    ööö

---
+++ WS1/ws1.js
@@ -0,0 +1,42 @@
+var http = require("http");
+http
+  .createServer(function(request, response) {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.write(`\   
+    <html>\n
+		<head>\n
+			<style type=text/css>\n
+				th,td {border-color:black;border-style:solid;border-width:1px;}\n
+			</style>\n
+        </head>\n
+        <body>\n
+            <table style="width:400px;border-color:black;border-style:solid;border-width:1px">\n
+				<tr>\
+                    <th>Name</th>\
+                    <th>Item</th> \
+                    <th>Material</th>\
+                </tr>\
+                <tr>\
+                    <td>Tom</td>\
+                    <td>Shirt</td>\
+                    <td>Cotton</td>\
+                </tr>\
+                <tr>\
+                    <td>Matt</td>\
+                    <td>Pants</td>\
+                    <td><a href="https://en.wikipedia.org/wiki/Wool">wool</a></td>\
+                </tr>\
+                <tr>\
+                    <td>John</td>\
+                    <td>Jacket</td>\
+                    <td>casmere wool</a></td>\
+                </tr>\
+            </table>\
+        </body>\
+    </html>
+    `);
+    response.end();
+  })
+  .listen(8081);
+
+console.log("Server running at http://127.0.0.1:8081/");
